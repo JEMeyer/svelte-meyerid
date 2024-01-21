eefecccac9834938552e119c3b4a051f22e0fbea
Joe Meyer <joseph@meyer.id>
2024-01-21 16:33:43 -0600

    add flowbit-svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -35,7 +35,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/JEMeyer/svelte-meyerid">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
